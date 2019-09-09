c928ec6d5847bc03cd6fb526c2a4092674508213
Awesomak <awesomebest2@gmail.com>
2019-09-09 15:10:49 +0300

    start

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const port = process.env.PORT || 5000;
-const io = require('socket.io')(port);
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.listen(port, () => console.log(`Server started on port ${port}`));
+const io = require('socket.io').listeb(app);
+
+//Production settings
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 let message = [ '' ];
 let pos = message.length - 1;
