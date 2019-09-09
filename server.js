92794f5a095fd871c5847340c8b8848a72c38e19
Awesomak <awesomebest2@gmail.com>
2019-09-09 15:22:37 +0300

    start

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const port = process.env.PORT || 5000;
 const express = require('express');
 const path = require('path');
-
+const http = require('http');
 const app = express();
+const server = http.createServer(app);
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
-const io = require('socket.io').listeb(app);
+const io = require('socket.io').listen(server);
 
 //Production settings
 if (process.env.NODE_ENV === 'production') {
